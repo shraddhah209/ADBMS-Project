52c879b7f1b690cfc0216e39abf1af8f99e86c56
surmeet <surmeetkaur.95@gmail.com>
2016-09-13 00:42:17 +0530

    Teacher's table + studentDB view and template

--- teachers/models.py
+++ teachers/models.py
@@ -1,16 +1,37 @@
 from django.db import models
 
-class student_labs(models.Model):
-    student_name = models.CharField(max_length=60)
-    Exp1 = models.IntegerField(max_length=3)
-    Exp2 = models.IntegerField(max_length=3)
-    Exp3 = models.IntegerField(max_length=3)
-    Exp4 = models.IntegerField(max_length=3)
-    Exp5 = models.IntegerField(max_length=3)
-    Exp6 = models.IntegerField(max_length=3)
-    Exp7 = models.IntegerField(max_length=3)
-    Exp8 = models.IntegerField(max_length=3)
-    Exp9 = models.IntegerField(max_length=3)
-    Exp10 = models.IntegerField(max_length=3)
-    avg = models.IntegerField(max_length=3)
+class Students(models.Model):
+
+    student_roll = models.AutoField(primary_key=True, unique=True, editable=False)
+    student_name = models.CharField(max_length=70)
+    Exp1 = models.IntegerField(default=0)
+    Exp2 = models.IntegerField(default=0)
+    Exp3 = models.IntegerField(default=0)
+    Exp4 = models.IntegerField(default=0)
+    Exp5 = models.IntegerField(default=0)
+    Exp6 = models.IntegerField(default=0)
+    Exp7 = models.IntegerField(default=0)
+    Exp8 = models.IntegerField(default=0)
+    Exp9 = models.IntegerField(default=0)
+    Exp10 = models.IntegerField(default=0)
+    exp_avg = models.IntegerField(default=0)
+    T1q1 = models.IntegerField(default=0)
+    T1q2 = models.IntegerField(default=0)
+    T1q3 = models.IntegerField(default=0)
+    T1q4 = models.IntegerField(default=0)
+    T1_total = models.IntegerField(default=0)
+    T2q1 = models.IntegerField(default=0)
+    T2q2 = models.IntegerField(default=0)
+    T2q3 = models.IntegerField(default=0)
+    T2q4 = models.IntegerField(default=0)
+    T2_total = models.IntegerField(default=0)
+    TT_avg = models.IntegerField(default=0)
+    A1 = models.IntegerField(default=0)
+    A2 = models.IntegerField(default=0)
+    Ass_avg = models.IntegerField(default=0)
+    attendance = models.IntegerField(default=0)
+    final_marks = models.IntegerField(default=0)
+    def __str__(self):
+        return str(self.student_roll)+". "+self.student_name
+
 
--- teachers/views.py
+++ teachers/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
+from .models import Students
 
-# Create your views here.
+
+def studentDB(request):
+    all_students = Students.objects.all()
+    template = loader.get_template("teachers/studentData.html")
+    context = {
+        "all_stud": all_students,
+    }
+    return HttpResponse(template.render(context,request))
+
+
+def detail(request, student_roll):
+    return HttpResponse("roll no is : "+str(student_roll)+"       name is : ")
