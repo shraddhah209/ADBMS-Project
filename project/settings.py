bba7cf9ca69e24300a4a054f596d5a031865a9ee
shraddhah209 <shraddhaholmukhe96@gmail.com>
2016-09-13 01:51:30 +0530

    new app copo with 3 models coadbms , coatdadbm and po Views yet to be worked on.

--- project/settings.py
+++ project/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'copo.apps.CopoConfig',
     'teachers.apps.TeachersConfig',
     'django.contrib.admin',
     'django.contrib.auth',
