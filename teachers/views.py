d5183a6242e30dcf1ef8408ad2f2d5cd57c98a9e
surmeet <surmeetkaur.95@gmail.com>
2016-09-13 18:51:20 +0530

    studentData.html changed to add Exp 1 and Exp 2 marks. Same changes made in  commented part of views

--- teachers/views.py
+++ teachers/views.py
@@ -5,11 +5,17 @@ from .models import Students
 
 def studentDB(request):
     all_students = Students.objects.all()
+  #  html = ""
+  #  for blah in all_students:
+  #      html += str(blah.T1q1)+"      "
+  #    return HttpResponse(html)
     template = loader.get_template("teachers/studentData.html")
     context = {
         "all_stud": all_students,
     }
     return HttpResponse(template.render(context,request))
+
+
 
 
 def detail(request, student_roll):
