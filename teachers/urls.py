76a063760503152fa767df059043e2bb1d140e16
surmeet <surmeetkaur.95@gmail.com>
2016-10-18 02:41:37 +0530

    bullshit

--- teachers/urls.py
+++ teachers/urls.py
@@ -9,6 +9,8 @@ urlpatterns = [
     url(r'^studentDB$', views.studentDB, name='studentDB'),
     url(r'^(?P<student_roll>[0-9]+)/$', views.detail, name='detail'),
     url(r'^login/$', views.UserFormView.as_view(), name='login'),
+    url(r'^save$', views.save, name='save')
+   # url(r'^bullshit$', views.bullshit, name='bullshit')
 ]
 
 if settings.DEBUG:
--- teachers/views.py
+++ teachers/views.py
@@ -1,9 +1,10 @@
 from django.http import HttpResponse, HttpResponseRedirect
-from django.shortcuts import render_to_response,get_object_or_404
+from django.shortcuts import render_to_response,get_object_or_404,render, redirect
 from django.template import loader
-from django.shortcuts import render, redirect
 from django.contrib import auth
 from django.contrib.auth import authenticate, login
+from django.urls import reverse
+from django.template import RequestContext
 from django.views import generic
 from django import forms
 from django.views.generic import View
@@ -20,13 +21,55 @@ def studentDB(request):
     credentials = ServiceAccountCredentials.from_json_keyfile_name('c:/Users/Sarbjit/Desktop/Adbms-7c8aa9cf0720.json', scope)
     gc = gspread.authorize(credentials)
     wks = gc.open("test").sheet1
-    all_students = Students.objects.all()
-    count = len(all_students)
-    blah = wks.col_values(2)
-    stud = Students(pk=(count+1))
-    stud.student_name = blah
-    stud.save()
-    return render_to_response('teachers/studentData.html', {'wks':count})
+    student_list = ["student_roll","student_name","Exp1"]
+    blah = Students.objects.all()
+    count = len(blah)
+    return render(request,'teachers/studentData.html', {'wks':count})
+
+def save(request):
+    json_key = 'Adbms-7c8aa9cf0720.json'
+    scope = ['https://spreadsheets.google.com/feeds']
+    credentials = ServiceAccountCredentials.from_json_keyfile_name('c:/Users/Sarbjit/Desktop/Adbms-7c8aa9cf0720.json',scope)
+    gc = gspread.authorize(credentials)
+    wks = gc.open("test").sheet1
+    count = wks.rows_count
+    c = 2
+    while c<=count:
+        all_students = Students.objects.filter(pk=c-1)
+        for a in all_students:
+            a.student_roll != wks.acell('A' + str(c)).value
+            a.student_name = wks.acell('B'+ str(c)).value
+            a.Exp1 = wks.acell('C' + str(c)).value
+            a.Exp2 = wks.acell('D' + str(c)).value
+            a.Exp3 = wks.acell('E' + str(c)).value
+            a.Exp4 = wks.acell('F' + str(c)).value
+            a.Exp5 = wks.acell('G' + str(c)).value
+            a.Exp6 = wks.acell('H' + str(c)).value
+            a.Exp7 = wks.acell('I' + str(c)).value
+            a.Exp8 = wks.acell('J' + str(c)).value
+            a.Exp9 = wks.acell('K' + str(c)).value
+            a.Exp10 = wks.acell('L' + str(c)).value
+            a.exp_avg = wks.acell('M' + str(c)).value
+            a.T1q1 = wks.acell('N' + str(c)).value
+            a.T1q2 = wks.acell('O' + str(c)).value
+            a.T1q3 = wks.acell('P' + str(c)).value
+            a.T1q4 = wks.acell('Q' + str(c)).value
+            a.T1_total = wks.acell('R' + str(c)).value
+            a.T2q1 = wks.acell('S' + str(c)).value
+            a.T2q2 = wks.acell('T' + str(c)).value
+            a.T2q3 = wks.acell('U' + str(c)).value
+            a.T2q4 = wks.acell('V' + str(c)).value
+            a.T2_total = wks.acell('W' + str(c)).value
+            a.TT_avg = wks.acell('X' + str(c)).value
+            a.A1 = wks.acell('Y' + str(c)).value
+            a.A2 = wks.acell('Z' + str(c)).value
+            a.Ass_avg = wks.acell('AA' + str(c)).value
+            a.attendance = wks.acell('AB' + str(c)).value
+            a.final_marks = wks.acell('AC' + str(c)).value
+        c += 1
+        a.save()
+
+    return render_to_response('teachers/bullshit.html',{'val':a.student_name})
 
 def stud(request):
     all_students = Students.objects.all()
