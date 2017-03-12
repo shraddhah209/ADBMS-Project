84a761b7d2290450a2d07883fa1172625955334e
shraddhah209 <shraddhaholmukhe96@gmail.com>
2017-03-12 19:37:26 +0530

    Only front end changes required

--- project/urls.py
+++ project/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import include,url
 from django.contrib import admin
-
+#urls main
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^teachers/', include("teachers.urls")),
